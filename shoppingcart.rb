360692a327e4ec5622ed111929eef8c96a6eaae8
Konstantinos Gkavas <gavaskonstantinos@gmail.com>
2022-08-27 02:20:07 +0200

    Added Shoppingcart class
    Added shoppingcart.rb and 'require'd it on main.rb. Created a clothing.rb which might deem useless

---
+++ shoppingcart.rb
@@ -0,0 +1,46 @@
+class Shoppingcart
+    attr_accessor :contents
+
+    def initialize
+        @contents = {
+            'jeans': 0,
+            'tshirts': 0,
+            'sweaters': 0
+        }
+    end
+
+    def addjeanstocart
+        @contents['jeans'] += 1
+    end
+
+    def addtshirttocart
+        @contents['tshirts'] += 1
+    end
+
+    def addsweatertocart
+        @contents['sweaters'] += 1
+    end
+
+    def estimatetotalcost
+        cost = 0
+        #There is no promotion for jeans
+        cost += @contents['jeans'] * 50
+        
+        #If you buy more than two sweaters, you will have a 5% discount on the sweaters
+        if @contents['sweaters'] > 2
+            cost += 0.95 * (@contents['sweaters'] * 20)
+        else
+            cost += @contents['sweaters'] * 20
+        end
+        
+        #For every t-shirt that you buy, you will get a second t-shirt for free
+        if @contents['tshirts'] >= 2
+            cost += (@contents['tshirts'] / 2) * 15
+            cost += (@contents['tshirts'] % 2) * 15
+        else
+            cost += @contents['tshirts'] * 15
+        end
+        
+        return cost
+    end
+end
