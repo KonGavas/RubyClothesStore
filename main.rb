1c46080a2eed105e4ca6dbb530833e51dee016db
Konstantinos Gkavas <gavaskonstantinos@gmail.com>
2022-08-29 03:16:25 +0200

    Updated main.rb to accept command-line arguments
    main.rb can be called with some variables which are accessed using 'ARGV'. This is to accommodate for tests performed with rake

--- main.rb
+++ main.rb
@@ -7,43 +7,74 @@ shoppingcart = Shoppingcart.new()
 
 AppInterface.welcometext
 
-# print "\nChoice: "
-# if ARGV[0].nil?
-#     input = gets.chomp().to_i
-# else
-#     #This is for using rake to run bad-input tests.
-#     #Makes it so we can pass an argument to main.rb when calling it.
-#     input = ARGV[0].to_i
-# end
+if not ARGV[0].nil?
+    #This is for using rake to run bad-input tests.
+    #Makes it so we can pass arguments to main.rb when running it.
 
-input = 0
-while input != 6
-    AppInterface.displaycartcontents(shoppingcart)
+    if not (ARGV.include?('6'))
+        #Append 6 as the last choice so that the application exits
+        ARGV.push("6")
+    end
+    puts "ARGV: #{ARGV}"
+
     AppInterface.menutext
 
-    print "\nChoice: "
-    input = gets.chomp().to_i
-    
-    system('clear') || system('cls')
-    AppInterface.welcometext
-
-    case input
-    when 1
-        shoppingcart.addjeanstocart
-    when 2
-        shoppingcart.addtshirttocart
-    when 3
-        shoppingcart.addsweatertocart
-    when 5
-        AppInterface.showpromotions
-    when 6
-        next
-    else
-        puts "\nPlease insert a valid input."
+    for choice in ARGV
         puts ""
+        AppInterface.displaycartcontents(shoppingcart)
+
+        puts "\nChoice: #{choice}"
+
+        case choice
+        when '1'
+            shoppingcart.addjeanstocart
+        when '2'
+            shoppingcart.addtshirttocart
+        when '3'
+            shoppingcart.addsweatertocart
+        when '5'
+            AppInterface.showpromotions
+        when '6'
+            #Checkout
+            totalcost = shoppingcart.estimatetotalcost
+            AppInterface.checkout(totalcost)
+            break
+        else
+            puts "\nPlease insert a valid input."
+        end
     end
+else
+    input = 0
+    while input != 6
+        AppInterface.displaycartcontents(shoppingcart)
+        AppInterface.menutext
+
+        print "\nChoice: "
+        input = gets.chomp().to_i
+        
+        system('clear') || system('cls')
+        AppInterface.welcometext
+
+        case input
+        when 1
+            shoppingcart.addjeanstocart
+        when 2
+            shoppingcart.addtshirttocart
+        when 3
+            shoppingcart.addsweatertocart
+        when 5
+            AppInterface.showpromotions
+        when 6
+            next
+        else
+            puts "\nPlease insert a valid input."
+            puts ""
+        end
+    end
+
+    #Checkout
+    totalcost = shoppingcart.estimatetotalcost
+    AppInterface.checkout(totalcost)
 end
 
-#Checkout
-totalcost = shoppingcart.estimatetotalcost
-AppInterface.checkout(totalcost)
+exit
